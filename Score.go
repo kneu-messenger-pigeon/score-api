60a0dbb655d2285e3b8261c1b3b14beab1107a49
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-01-23 12:36:52 +0200

    Split Score to First Score and to Second Score

--- Score.go
+++ Score.go
@@ -1,8 +1,8 @@
 package scoreApi
 
 type Score struct {
-	Lesson     Lesson  `json:"lesson,omitempty"`
-	LessonHalf int     `json:"lessonHalf"`
-	Score      float32 `json:"score"`
-	IsAbsent   bool    `json:"isAbsent"`
+	Lesson      Lesson  `json:"lesson,omitempty"`
+	FirstScore  float32 `json:"firstScore"`
+	SecondScore float32 `json:"secondScore"`
+	IsAbsent    bool    `json:"isAbsent"`
 }
