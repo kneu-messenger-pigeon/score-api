4f16b499762682269364c68ae38b983eab380cf9
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-05-21 16:48:29 +0300

    Update DisciplineScore

--- DisciplineScore.go
+++ DisciplineScore.go
@@ -2,5 +2,5 @@ package scoreApi
 
 type DisciplineScore struct {
 	Discipline Discipline `json:"discipline"`
-	Score      Score      `json:"scores,omitempty"`
+	Score      Score      `json:"score"`
 }
--- Score.go
+++ Score.go
@@ -1,8 +1,8 @@
 package scoreApi
 
 type Score struct {
-	Lesson      Lesson  `json:"lesson,omitempty"`
-	FirstScore  float32 `json:"firstScore"`
-	SecondScore float32 `json:"secondScore"`
-	IsAbsent    bool    `json:"isAbsent"`
+	Lesson      Lesson   `json:"lesson,omitempty"`
+	FirstScore  *float32 `json:"firstScore"`
+	SecondScore *float32 `json:"secondScore"`
+	IsAbsent    bool     `json:"isAbsent"`
 }
