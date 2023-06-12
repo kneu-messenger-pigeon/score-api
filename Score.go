06733e39e1f68a27af42c29b5530664a2d2c3034
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-06-12 11:43:00 +0300

    Score.IqEqual method

--- Score.go
+++ Score.go
@@ -10,3 +10,15 @@ type Score struct {
 func (score *Score) IsDeleted() bool {
 	return score.IsAbsent == false && score.FirstScore == nil && score.SecondScore == nil
 }
+
+func (score *Score) IsEqual(other *Score) bool {
+	return score.IsDeleted() == other.IsDeleted() &&
+		score.IsAbsent == other.IsAbsent &&
+		compareScoreValue(score.FirstScore, other.FirstScore) &&
+		compareScoreValue(score.SecondScore, other.SecondScore)
+}
+
+func compareScoreValue(this *float32, other *float32) bool {
+	return (this == nil) == (other == nil) && // one of them is not nil
+		(this == other || *this == *other) // both are nil or have same value
+}
--- Score_test.go
+++ Score_test.go
@@ -1,6 +1,8 @@
 package scoreApi
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestScore_IsDeleted(t *testing.T) {
 	t.Run("deleted", func(t *testing.T) {
@@ -38,4 +40,183 @@ func TestScore_IsDeleted(t *testing.T) {
 			t.Errorf("Expected not deleted score, score: %v ", score)
 		}
 	})
+}
+
+func TestScore_IsEqual(t *testing.T) {
+	t.Run("same_scores", func(t *testing.T) {
+		expectedIsEqual := true
+
+		t.Run("with_both_values", func(t *testing.T) {
+			leftScore := &Score{
+				FirstScore:  floatPointer(1.2),
+				SecondScore: floatPointer(9),
+			}
+
+			rightScore := &Score{
+				FirstScore:  floatPointer(1.2),
+				SecondScore: floatPointer(9),
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be equal, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("with_first_value", func(t *testing.T) {
+			leftScore := &Score{
+				FirstScore: floatPointer(1.2),
+			}
+
+			rightScore := &Score{
+				FirstScore: floatPointer(1.2),
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be equal, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("with_second_values", func(t *testing.T) {
+			leftScore := &Score{
+				SecondScore: floatPointer(1.2),
+			}
+
+			rightScore := &Score{
+				SecondScore: floatPointer(1.2),
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be equal, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("with_absents", func(t *testing.T) {
+			leftScore := &Score{
+				IsAbsent: true,
+			}
+
+			rightScore := &Score{
+				IsAbsent: true,
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be equal, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("deleted", func(t *testing.T) {
+			leftScore := &Score{}
+			rightScore := &Score{}
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be equal, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+	})
+
+	t.Run("different", func(t *testing.T) {
+		expectedIsEqual := false
+
+		t.Run("with_both_values", func(t *testing.T) {
+			leftScore := &Score{
+				FirstScore:  floatPointer(5.2),
+				SecondScore: floatPointer(9),
+			}
+
+			rightScore := &Score{
+				FirstScore:  floatPointer(9),
+				SecondScore: floatPointer(9),
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be diff, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("with_both_values", func(t *testing.T) {
+			leftScore := &Score{
+				FirstScore:  floatPointer(9),
+				SecondScore: floatPointer(5.2),
+			}
+
+			rightScore := &Score{
+				FirstScore:  floatPointer(9),
+				SecondScore: floatPointer(9),
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be diff, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("with_first_value", func(t *testing.T) {
+			leftScore := &Score{
+				FirstScore: floatPointer(1.2),
+			}
+
+			rightScore := &Score{
+				FirstScore: floatPointer(4.2),
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be diff, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("with_second_values", func(t *testing.T) {
+			leftScore := &Score{
+				SecondScore: floatPointer(4.2),
+			}
+
+			rightScore := &Score{
+				SecondScore: floatPointer(1.2),
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be diff, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("with_different_keys", func(t *testing.T) {
+			leftScore := &Score{
+				FirstScore: floatPointer(1.2),
+			}
+
+			rightScore := &Score{
+				SecondScore: floatPointer(1.2),
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be diff, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("with_absents", func(t *testing.T) {
+			leftScore := &Score{
+				IsAbsent: true,
+			}
+
+			rightScore := &Score{
+				IsAbsent: false,
+			}
+
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be diff, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+
+		t.Run("deleted", func(t *testing.T) {
+			leftScore := &Score{}
+			rightScore := &Score{
+				SecondScore: floatPointer(2),
+			}
+			if leftScore.IsEqual(rightScore) != expectedIsEqual {
+				t.Errorf("Expected to be diff, score left: %v == score right %v ", leftScore, rightScore)
+			}
+		})
+	})
+
+}
+
+func floatPointer(value float32) *float32 {
+	return &value
 }
